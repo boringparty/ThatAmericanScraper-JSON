31ecc933e41fb97dccec26a60f1061c77e1a36cc
boringparty <1559567+boringparty@users.noreply.github.com>
2026-04-27 02:50:01 -0700

    Update generateFeed_oldies.py

--- script/generateFeed_oldies.py
+++ script/generateFeed_oldies.py
@@ -230,7 +230,7 @@ def main():
         url = enclosure.attrib.get("url")
 
         if url:
-            download_line = f"<a href="{url}">download</a>"
+            download_line = f"{url}"
 
             match = re.search(
                 r"(<description><!\[CDATA\[)(.*?)(\]\]></description>)",
